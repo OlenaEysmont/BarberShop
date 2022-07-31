091eec26c19fc393001119563280cef00026e56a
AndrieievaLena <elenakovalevamod@gmail.com>
2022-07-31 23:02:25 +0300

    finished mobile

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,10 +9,10 @@
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
   
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+      // const scrollLockMethod = !isMenuOpen
+        // ? 'disableBodyScroll'
+      //   : 'enableBodyScroll';
+      // bodyScrollLock[scrollLockMethod](document.body);
       
      
      
@@ -25,10 +25,10 @@
   
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
+    // window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    //   if (!e.matches) return;
+    //   mobileMenu.classList.remove('is-open');
+    //   openMenuBtn.setAttribute('aria-expanded', false);
+    //   bodyScrollLock.enableBodyScroll(document.body);
+    // });
   })();
